13fa977c796ec5e195edb61ff53cfe24980b603d
Jim Clark <jim@atomist.com>
2020-12-02 18:12:44 -0800

    Remove deploy repos

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repositories [["releases" {:url "https://sforzando.jfrog.io/sforzando/libs-release-local"
-                              :username [:env/mvn_artifactorymavenrepository_user]
-                              :password [:env/mvn_artifactorymavenrepository_pwd]}]]
   :repl-options {:init-ns clj-test-lib.core})
